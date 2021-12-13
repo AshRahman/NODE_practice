a1691e25ec7fbb78bd5341b87bd82506d0f493f4
AshRahman <rockashfaq0@outlook.com>
2021-12-13 17:16:58 +0600

    learned module system

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const _lodash = require('lodash');
+// setTimeout(() => {
+//     console.log('test');
+// }, 1000);
+const _ = require('lodash');
+const people = require('./people');
+
+console.log(_.last(people));
---
+++ people.js
@@ -0,0 +1,5 @@
+const people = ['rahat', 'ashfaq'];
+
+module.exports = people;
+
+//* Module wrapper function---------->vanilla iffy
